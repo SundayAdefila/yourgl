8fcff35320672c1aa95d3e6683d04a27a7a1b1be
Sunday Adefila <adefilaedward@gmail.com>
2017-06-25 21:52:24 +0100

    validates_uniqueness_of :page_url

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,7 @@
 class Content < ApplicationRecord
   attr_accessor :error
   validates_presence_of :page_url
+  validates_uniqueness_of :page_url
   validate :page_parsed
 
   def page_parsed
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Content, type: :model do
   it { should validate_presence_of(:page_url) }
+  it { should validate_uniqueness_of(:page_url) }
 end
